2ce11ac92cc9a93cc7bdc8c2f6f7bdf269694b45
JOASPER SCOTTAN FELIZ CUEVAS <78002056+Joasper@users.noreply.github.com>
2026-03-05 18:45:57 -0400

    Entidad Organizacion

--- MikroClean.Domain/Entities/Base/BaseEntity.cs
+++ MikroClean.Domain/Entities/Base/BaseEntity.cs
@@ -3,5 +3,7 @@
     public class BaseEntity
     {
         public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime DeletedAt { get; set; } = DateTime.UtcNow;
     }
 }
---
+++ MikroClean.Domain/Entities/Organizations.cs
@@ -0,0 +1,13 @@
+﻿using MikroClean.Domain.Entities.Base;
+using System.Numerics;
+
+namespace MikroClean.Domain.Entities
+{
+    public class Organizations : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Contact_email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
