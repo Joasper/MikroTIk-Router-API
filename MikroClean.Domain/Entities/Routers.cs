4f988738763c31f807087e581ac52bb0fc10dac2
elidudominici-del <elidudominici@gmail.com>
2026-03-07 00:15:00 -0400

    👍

--- MikroClean.Domain/Entities/Routers.cs
+++ MikroClean.Domain/Entities/Routers.cs
@@ -1,27 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MikroClean.Domain.Entities.Base;
 
-namespace MikroClean.Domain.Entities
-{
-    internal class Routers:BaseEntity
-    {
-        public string Name { get; set; }
-        public string IPAddress { get; set; }
-        public int Organization_Id { get; set; }
-        public string User { get; set; }
-        public string EncryptedPassword { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedAt { get; set; }
-    }
-}
-=======
-﻿using MikroClean.Domain.Entities.Base;
+using MikroClean.Domain.Entities.Base;
 
 public class Router : BaseEntity
 {
@@ -37,4 +15,3 @@ public class Router : BaseEntity
 
     public int OrganizationId { get; set; }
 }
->>>>>>> e1056afd14ae74e31ac22d88efc9bbabc0f9e09a
